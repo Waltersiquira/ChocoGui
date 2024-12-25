9ed5eb4a1ab43423e942d45d79f52232e3e02803
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-25 11:18:53 -0300

    Create form-chocolate.php

---
+++ form-chocolate.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChocoGui</title>
+</head>
+<body>
+    <form action="enviar-form-chocolate.php" method="post">
+        digite nome do chocolate <br>
+        <input type="text" name="nome"> <br>
+        digite especificações do chocolate <br>
+        <input type="text" name="especificacoes"> <br>
+        digite o preço do chocolate <br>
+        <input type="text" name="preco"> <br>
+        digite a imagem do chocolate <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
